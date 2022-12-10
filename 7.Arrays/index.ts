83891d66dc75731d2493d89a8430af086ba3eec7
Carnifex <shirov.ivan@vk.com>
2022-12-11 03:04:35 +0500

    arr

---
+++ 7.Arrays/index.ts
@@ -0,0 +1,11 @@
+const cats: string[] = ['Барсик', 'Петька', 'Борька'];
+const ageCats: number[] = [11, 5, 15];
+const arr: any[] = [1, true, 'Барсик'];
+
+const barsik = cats[0];
+
+// НИЗЯ! cats.push(2);
+cats.push("Можно");
+
+// массив массивов
+const counter: number[][] = [[11, 5, 15], [11, 9, 15], [55, 5, 15]]
