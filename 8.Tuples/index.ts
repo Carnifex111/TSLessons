b2c284cb87db03c0de3ef9fbeb8bef5f405388f2
Carnifex <shirov.ivan@vk.com>
2022-12-12 21:24:45 +0500

    tuples

---
+++ 8.Tuples/index.ts
@@ -0,0 +1,21 @@
+// кортежи
+
+const catDataTuple: [number, string, boolean] = [12, "Барсик", true];
+
+// catDataTuple[0] = '1';
+
+// можно пушнуть но обратиться не можем, так как будет только 3 эемента в этом порядке
+// кортеж - неизменяемая структура с определенным порядком
+// catDataTuple.push(12);
+// catDataTuple[3]; //низя
+
+//получим массив в котором получим лишь строки
+const res = catDataTuple.map((el) => `${el} - data`);
+
+//деструктуризация
+const [age, catname, alive] = catDataTuple;
+
+//спрет оператор
+//мы развернули массив в конце
+//за счет этого мы можем расширять сколько угодно его, но в формате string
+const student: [string, string, number, ...string[]] = ['Широв', 'Иван', 26, 'Group-4']
